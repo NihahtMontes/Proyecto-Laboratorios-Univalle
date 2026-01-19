8c0deb5e1b92ada8a342fb956840d1420126549f
Nihaht Poly Montes Peñarrieta <mpn0036072@est.univalle.edu>
2026-01-18 23:02:23 -0300

    Normalize line endings via .gitattributes

--- Models/Enums/GeneralStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Laboratorios_Univalle.Models.Enums
-{
-    public enum GeneralStatus
-    {
-        Activo = 0,
-        Inactivo = 1,
-        Eliminado = 2
-    }
-}
--- Models/Enums/ServiceType.cs
+++
@@ -1,8 +0,0 @@
-namespace Proyecto_Laboratorios_Univalle.Models.Enums
-{
-    public enum ServiceType
-    {
-        Internal = 0,
-        External = 1
-    }
-}
